626602b4a6915db3e9181819c96f42b878eb7bca
Peggie Liang <liangp227@gmail.com>
2020-09-26 00:25:26 -0400

    Initial Commit

---
+++ flixapp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  flixapp
+//
+//  Created by Peggie Liang on 9/26/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
